52743fe4586427b82cd2ed2192db3ab50ca005b4
fabeyor <fabeyor@gmail.com>
2022-11-29 13:59:43 +0100

    Update ehistory.sh

--- ehistory.sh
+++ ehistory.sh
@@ -3,4 +3,4 @@
 # Reference: https://github.com/fabeyor/ehistory
 # Website: https://fabeyor.medium.com
 
-HISTTIMEFORMAT="TimeStamp: %m/%d/%Y %H:%M | SrcIP: $(w | grep -Eo '(^| )(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])($|[[:space:]])')  | Logon User: $USER | Command: "
+HISTTIMEFORMAT="TimeStamp: %m/%d/%Y %H:%M  |  SrcIP: $(w | grep -Eo '(^| )(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])($|[[:space:]])')  |  Logon User: $USER  |  Command:  "
